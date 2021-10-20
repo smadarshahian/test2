16b5706a349a29f4b5edc3119986c9f8cf7b83c1
smadarshahian <s.madarshahian@juston.com>
2021-10-20 16:16:57 +0200

    hellllooooooo

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.print("Hello World");
+        System.out.println("java");
+
+        var text = "Planet";
+        var year = 2020;
+        System.out.println("year = " + year);
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("HelloWorld.main");
+        System.out.println("text = " + text);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("i = " + i);
+        }
+
+
+
+
+
+    }
+
+    public static final int staticfeld = 100;
+
+
+}
