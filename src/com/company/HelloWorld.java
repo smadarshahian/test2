e191b64366d544d875624f302bc4133d61ae42a8
smadarshahian <s.madarshahian@juston.com>
2021-10-21 09:22:24 +0200

    new commit für delete

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -19,7 +19,9 @@ public class HelloWorld {
         for (int i = 0; i < 3; i++) {
             System.out.println("i = " + i);
         }
-
+        for (int j = 0; j < 3; j++) {
+            System.out.println("j = " + j);
+        }
 
 
 
